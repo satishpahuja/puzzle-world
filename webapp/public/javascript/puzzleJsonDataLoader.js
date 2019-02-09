90a3544aa12fcbec6185973d51092fea7c528e44
Satish Pahuja <satishpahuja91@gmail.com>
2019-02-09 14:13:57 +0530

    Correcting puzzle

--- webapp/public/javascript/puzzleJsonDataLoader.js
+++ webapp/public/javascript/puzzleJsonDataLoader.js
@@ -256,14 +256,12 @@ function fillAndReturnInterviewPuzzles() {
 
 			"puzzleHeading": "Deadly Choice Puzzle",
 
-			"puzzleQuestion": "A man is caught on the King's property."+
+			"puzzleQuestion": "A man is caught on the Kings property."+
 			"He is brought before the King to be punished."+ 
 			"The King says, You must give me a statement"+
 			"If it is true, you will killed by lions."+
 			"If it is false, you will be killed by trampling of wild buffalo."+
-			
 			"But in the end, the King has to let the man go."+
-			
 			"What was the man's statement?",
 
 			"puzzleSolution": "I will be killed by trampling of wild buffalo."+
@@ -272,7 +270,6 @@ function fillAndReturnInterviewPuzzles() {
 			"because if so the man should be killed by the lions."+
 			"But if he is killed by the lions, then his statement would be false,"+
 			"and so should have been trampled by buffalo."+
-			
 			"So the King can't tell if it is a lie or truth, so decides to just let him go.<br/>",
 
 			"puzzleInsertionDate": "24.01.2019"
